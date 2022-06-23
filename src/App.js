b26f87396236ba911f6dcff8e65d1a3dd6aedf4f
Drew Knoeller <drewk2629@gmail.com>
2022-06-23 19:27:14 -0400

    added styling and hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import './App.css';
+import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'Commercial',
+      descripton: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { 
+      name: 'Portraits', 
+      descripton: 'Portraits of people in my life'
+    },
+    {
+      name: 'Food',
+      descripton: 'Delicious delicacies'
+    },
+    {
+      name: 'Landscape',
+      descripton: 'Fields, farmhouses, waterfalls, and the beauty of nature'
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}>
+      </Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
